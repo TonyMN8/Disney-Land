b8b1fa462613912a8ed8f80f4904302777d2ee75
adrian <garcipro2@gmail.com>
2025-12-09 17:36:49 +0100

    Codigo principal en visitante_model.py

--- models/visitante_model.py
+++ models/visitante_model.py
@@ -1,20 +1,15 @@
-# importamos el modelo base:
 from models.base_model import BaseModel
-# importamos las dependencias.
 from peewee import *
 from playhouse.postgres_ext import BinaryJSONField
 from datetime import datetime
 
 class VisitanteModel(BaseModel):
-    id = AutoField()
+    id = AutoField(primary_key=True)
     nombre = CharField(null=False)
-    email = CharField(unique=True, constraints=[Check("email LIKE '%@%'")])
+    email = CharField(unique=True)
     altura = IntegerField()
     fecha_registro = DateTimeField(default=datetime.now)
-    preferencias = BinaryJSONField(null=True, default=lambda: {
-        "tipo_favorito": "",
-        "restricciones": [""],
-        "historial_visitas": [
-            {"fecha": "", "atracciones_visitadas": ""}
-        ]
-    })
+    preferencias = BinaryJSONField(null=True, default={})
+    
+    class Meta:
+        table_name = 'visitantes'
