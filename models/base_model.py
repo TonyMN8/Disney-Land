d570e75fce7d329b433d1df56a862ac98390de7e
Tony <a.mitev098@gmail.com>
2025-12-16 14:55:45 +0100

    Add documentation: base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,14 @@
+# INFO: base_model.py 
+'''
+(*) Importamos la clase base Model de Peewee, es el ORM que usaremos para...
+... definir modelos (tablas) de la base de datos.
+'''
 from peewee import Model
 from database import db
 
+ # Defimos la clase BaseModel, es una clase base que heredará de peewee.
+    # Esta clase se usará como padre para todos los modelos de la aplicación.
+    
 class BaseModel(Model):
     class Meta:
         database = db
