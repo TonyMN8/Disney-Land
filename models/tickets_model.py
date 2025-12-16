9463c290369851404438854671e4b5268f71b242
Tony <a.mitev098@gmail.com>
2025-12-16 15:00:56 +0100

    Create complete module structure: tickets_model.py

---
+++ models/tickets_model.py
@@ -0,0 +1,32 @@
+# IMPORTACIONES.
+# Modelo Base.
+from models.base_model import BaseModel
+# Modelos relacionados con otras clases:
+from models.visitante_model import VisitanteModel
+from models.atraccion_model import AtraccionModel
+# Dependencias
+from peewee import * # type: ignore
+from playhouse.postgres_ext import BinaryJSONField # type: ignore
+from datetime import datetime
+
+# Modelo que representa la tabla "atracciones".
+# Hereda de BaseModel para usar la conexión a la base de datos.
+class TicketModel(BaseModel):
+    id = AutoField()
+    visitante = ForeignKeyField(VisitanteModel, backref='tickets', on_delete='CASCADE')
+    atraccion = ForeignKeyField(AtraccionModel, backref='tickets', null=True, on_delete='SET NULL')
+    fecha_compra = DateTimeField(default=datetime.now)
+    fecha_visita = DateField()
+    tipo_ticket = CharField(constraints=[Check("tipo_ticket IN ('general', 'colegio', 'empleado')")])
+    detalles_compra = BinaryJSONField(null=True, default=lambda: {
+        "precio": 0.0,
+        "descuentos_aplicados": [],
+        "servicios_extra": [],
+        "metodo_pago": ""
+    })
+    usado = BooleanField(default=False)
+    fecha_uso = DateTimeField(null=True)
+    
+    # Especificamos la tabla tickets en la base de datos.
+    class Meta:
+        table_name = 'tickets'
