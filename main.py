6ece49e3397fd660d25943bf7444ad582c915724
Tony <a.mitev098@gmail.com>
2025-12-19 18:41:11 +0100

    Updated: Minor changes to main.py to update the methods to static methods: main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from configuracion_db import inicializar_db
 from database import conectar_db, cerrar_db
-from menu.menu_visitantes import MenuVisitantes
+from menu.funciones import MenuPrincipal
 
 def main():
-    inicializar_db()
+    inicializar_db() 
     conectar_db()
-    menu = MenuVisitantes()
-    menu.mostrar()
-    cerrar_db()
+    
+    try:
+        MenuPrincipal.menu_principal() 
+    except Exception as e:
+        print("ERROR:", e)
+    finally:
+        cerrar_db()
 
 if __name__ == "__main__":
     main()
