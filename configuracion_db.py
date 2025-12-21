d42954b1fd3d91c88f850c7f5f9f58ea81eb1a88
Tony <a.mitev098@gmail.com>
2025-12-21 18:25:27 +0100

    Update: Now tables are deleted and created when the program starts: configuracion_db (inicializar_db)

--- configuracion_db.py
+++ configuracion_db.py
@@ -1,17 +1,18 @@
 from database import db
 from models.visitante_model import VisitanteModel
 from models.atraccion_model import AtraccionModel
+from models.tickets_model import TicketModel
 
 def inicializar_db():
     try:
-        db.connect(reuse_if_open=True)  # evita errores si ya hay conexión
-        
-        # safe=True evita errores si ya existe
-        db.create_tables([VisitanteModel], safe=True)  
-        db.create_tables([AtraccionModel], safe=True)
+        db.connect(reuse_if_open=True)
+
+        # Borrar tablas si existen.
+        db.drop_tables([TicketModel, AtraccionModel, VisitanteModel], safe=True)
+        print("INFO: Tablas eliminadas correctamente")
+
+        # Crear tablas nuevas
+        db.create_tables([VisitanteModel, AtraccionModel, TicketModel], safe=True)
         print("INFO: Tablas creadas correctamente")
     except Exception as e:
         print("ERROR al crear tablas:", e)
-    finally:
-        if not db.is_closed():
-            db.close()
